dd9fc5a71cd42aa9377a78a8d97d884b887daa09
adibaulia <adibaulia@gmail.com>
2017-06-07 19:53:59 +0700

    test

---
+++ public/home.blade.php
@@ -0,0 +1,21 @@
+@section('header')
+<h1>OKE OKE</h1>
+@endsection
+
+@section('main')
+
+    <h1><a class="main" href="/home/1">Berita Utama#1</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis nec augue sit amet tincidunt. Aenean nisi massa, maximus id consequat id, mollis sit amet orci. Integer ultricies massa risus, quis bibendum arcu placerat et. Quisque velit nulla, tempus ultrices mattis eu, porta et nibh. Donec laoreet sem in ante pellentesque, vitae finibus nibh elementum. Maecenas efficitur, lorem nec elementum condimentum, libero enim faucibus mauris, nec pretium tortor risus eu urna. Curabitur non neque egestas, eleifend lorem quis, convallis elit. Praesent pharetra dui massa, in congue odio mollis vitae. Praesent congue id nisl eget gravida. Aliquam ultrices tortor tortor, at facilisis eros gravida sed. Aenean varius lacus nulla, et fringilla ante tristique eu. Cras non felis nisi. Etiam ut pellentesque elit. Duis bibendum ipsum nec nunc egestas ornare ut id ligula. Pellentesque nunc purus, luctus et consequat vitae, vestibulum eget elit.</p>
+    <h1><a class="main" href="beritautama.html">Berita Utama#2</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis nec augue sit amet tincidunt. Aenean nisi massa, maximus id consequat id, mollis sit amet orci. Integer ultricies massa risus, quis bibendum arcu placerat et. Quisque velit nulla, tempus ultrices mattis eu, porta et nibh. Donec laoreet sem in ante pellentesque, vitae finibus nibh elementum. Maecenas efficitur, lorem nec elementum condimentum, libero enim faucibus mauris, nec pretium tortor risus eu urna. Curabitur non neque egestas, eleifend lorem quis, convallis elit. Praesent pharetra dui massa, in congue odio mollis vitae. Praesent congue id nisl eget gravida. Aliquam ultrices tortor tortor, at facilisis eros gravida sed. Aenean varius lacus nulla, et fringilla ante tristique eu. Cras non felis nisi. Etiam ut pellentesque elit. Duis bibendum ipsum nec nunc egestas ornare ut id ligula. Pellentesque nunc purus, luctus et consequat vitae, vestibulum eget elit.</p>
+
+@endsection
+
+@section('side')
+
+  <h1><a class="side" href="sidenews.html">Side News</a></h1>
+  <p class="side">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat consectetur suscipit. Aenean et erat aliquam, posuere orci vitae.</p>
+
+@endsection
+
+@extends('layouts.master')
