7ee6064a8c95c41b87347e3993b7c0702f5f7cd1
adibaulia <adibaulia@gmail.com>
2017-06-07 21:02:02 +0700

    layouting1

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('layouts/master');
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login/dashboard');
 });
