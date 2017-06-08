ebfd3b94e3a2ca70e1c88a01d1539ab287130574
Robit AlHazmi <robit.alhazmi@gmail.com>
2017-06-08 10:12:09 +0700

    create halaman login awal

--- resources/views/login/dashboard.blade.php
+++ resources/views/login/dashboard.blade.php
@@ -1,1 +1,12 @@
 @extends('layouts.noNav')
+
+@section('title')
+  Login Page
+@endsection
+
+@section('headerwrap')
+  <form class="" action="#" method="post">
+    <input type="text" name="username" value="">
+    <input type="password" name="password" value="">
+  </form>
+@endsection
