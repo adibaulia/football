031c7b3ef14a2a718b74465c8c288ed7baf98ccb
Robit AlHazmi <robit.alhazmi@gmail.com>
2017-06-09 21:03:14 +0700

    Login system sukses

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LoginController extends Controller
+{
+  public function login(Request $req)
+  {
+    $username = $req->input('username');
+    $password = $req->input('password');
+
+    $checkLogin = DB::table('admin')->where([
+      'username'  =>  $username,
+      'password'  =>  md5($password)
+    ])->get();
+
+    if (count($checkLogin) > 0) {
+      echo "Login Sukses";
+    }
+    else {
+      echo "Login Gagal";
+    }
+  }
+}
