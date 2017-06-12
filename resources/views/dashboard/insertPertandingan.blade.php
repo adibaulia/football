9393e5563d87ef8f50cd923d33bf1d5c361f9174
adibaulia <adibaulia@gmail.com>
2017-06-12 21:54:24 +0700

    okokoko

--- resources/views/dashboard/insertPertandingan.blade.php
+++ resources/views/dashboard/insertPertandingan.blade.php
@@ -9,43 +9,36 @@
 
         <div class="col-lg-8 col-lg-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Tambah Admin</div>
+                <div class="panel-heading">Tambah Pertandingan</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="/">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <label for="username" class="col-md-4 control-label">Username</label>
-
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Nama Pertandingan</label>
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
-
-                                @if ($errors->has('username'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
+                                <input  type="text" class="form-control" name="pertandingan" value="" required autofocus>
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Nama Stadion</label>
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input type="text" class="form-control" name="stadion" required>
+                            </div>
+                        </div>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Tanggal Pertandingan</label>
+                            <div class="col-md-6">
+                                <input type="date" class="form-control" name="tanggal" required>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-5 col-md-offset-6">
                                 <button type="submit" class="btn btn-primary">
-                                    SignUp!
+                                    Tambah Pertandingan
                                 </button>
 
                             </div>
